798f782c7685be05b5c64a99e4ebd24b0855676a
eddyg <eddygomet@gmail.com>
2024-02-24 11:33:02 +0100

    .fix serveur plus ecoute sur un fichier d'exemple

--- server/server.js
+++ server/server.js
@@ -1,30 +1,20 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-app.use(cors());
-
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: 'http://localhost:3000', methods: ["GET", "POST"] }
-});
-
-const PORT = process.env.PORT || 5000;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+    res.sendFile(__dirname + '/index.html');
+    //res.send('<h1>Hello World!</h1>');
 });
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
-    socket.emit('connection', null);
+io.on('connection', (socket) =>{
+    console.log('A client is connected');
 
     socket.on('disconnect', () => {
-        console.log('A user is disconnected');
-    });
+        console.log('A client is disconnected');
+    })
 });
 
-server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+http.listen(3000, ()=>{
+    console.log('Server running on 3000');
 });
